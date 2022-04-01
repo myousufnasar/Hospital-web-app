34ae6a9de4242ba44340c413f92f8e60dca9cb82
myousufnasar <myousufnasar3@gmail.com>
2022-04-01 14:55:55 +0300

    error fixed in db but sign up form not working

---
+++ app/models.py
@@ -0,0 +1,48 @@
+from . import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+import datetime
+from flask_login import UserMixin
+
+
+class Users (db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=True)
+    lastname = db.Column(db.String(50), nullable=True)
+    sex = db.Column(db.String(50), nullable=True)
+    address = db.Column(db.String(50), nullable=True)
+    dob = db.Column(db.String(50), nullable=True)
+    telephone = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    patient_image = db.Column(db.String(250), nullable=True)
+    password = db.Column(db.String(50), nullable=True) 
+    allergies = db.Column(db.String(500), nullable=True)
+    # change staff to patient later this is for testing purpose only
+    role = db.Column(db.String(), default='Staff')
+    medical_details = db.relationship('Medical_details', backref='users', uselist=False, lazy=True)
+    time = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+
+class Medical_details(db.Model):
+    id = db.Column(db.Integer,primary_key=True, nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    height = db.Column(db.Integer, nullable=True)
+    weight = db.Column(db.Integer, nullable=True)
+    last_visit = db.Column(db.Date, nullable=True)
+    date_created = db.Column(db.Date, nullable=True)
+    symptoms = db.Column(db.String(250), nullable=True)
+    diagnosis = db.Column(db.String(250), nullable=True)
+    med_img = db.Column(db.String(250), nullable=True)
+
+
+# class Appointments(db.Model):
+#     id = db.Column(db.Integer,primary_key=True, nullable=True)
+#     person_id = db.ForeignKey('patients_details.id')
+#     height = db.Column(db.Integer, nullable=True)
+#     weight = db.Column(db.Integer, nullable=True)
+#     last_visit = db.Column(db.Date, nullable=True)
+#     date_created = db.Column(db.Date, nullable=True)
+#     symptoms = db.Column(db.String(250), nullable=True)
+#     diagnosis = db.Column(db.String(250), nullable=True)
+#     med_img = db.Column(db.String(250), nullable=True)
---
+++ app/views.py
@@ -0,0 +1,87 @@
+from app import app
+from flask import  render_template, request, abort, flash, url_for, redirect
+from . import db
+from .models import Users
+from flask_login import login_user, login_required, logout_user, current_user
+
+@app.route("/")
+def home():
+    return render_template('index.html',  title="Home")
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+# route to signup users
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+   if request.method == "POST":
+      firstname = str(request.form.get('firstname')).title()
+    #   print(f"===========================> this is {firstname}")
+      lastname = str(request.form.get('lastname')).title()
+      sex = str(request.form.get('sex')).title()
+      address = str(request.form.get('address')).title()
+      dob = str(request.form.get('dob')).title()
+      email = str(request.form.get('email')).title()
+      telephone = str(request.form.get('telephone')).title()
+      password = str(request.form.get('password'))
+      
+    #   check for existence
+      check = Users.query.filter_by(email=email).first()
+      if check:
+            # print("==========================>User already exist")
+            flash("An Account has alredy been Registered with this email ")
+            abort(500)
+      else:
+        #   print("=======================>User saved")
+        #   saves user if not found
+          save_user = Users(firstname=firstname, lastname=lastname, email=email, password=password, telephone=telephone, address=address, sex=sex, dob=dob )
+          db.session.add(save_user)
+          db.session.commit()
+        
+
+          user = Users.query.filter_by(
+                email=email, password=password).first()
+          if user.role == 'Staff':
+                login_user(user, remember=True)
+                return redirect(url_for("staff"))
+
+
+
+
+   return render_template('signup.html', title="Sign Up")
+
+
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template('contacts.html')
+
+
+@app.route("/departments")
+def departments():
+    return render_template('departments.html')
+
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+
+@app.route("/appointments")
+def appointments():
+    return render_template('appointments.html')
+
+
